82847e1dbd3542696faf598d6b96c6c1741d3bde
Rüzgar Koray Genç <koray_genc1@hotmail.com>
2024-05-10 22:41:58 +0300

    updated dead

--- Assets/Prefabs/dead.cs
+++ Assets/Prefabs/dead.cs
@@ -7,7 +7,7 @@ public class dead : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("rüzxgarý sikkkkk");
+        Debug.Log("rüzxgasdasadadsdaaasadsad sikkkkk");
     }
 
     // Update is called once per frame
