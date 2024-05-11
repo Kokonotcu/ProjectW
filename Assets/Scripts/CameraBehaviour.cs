e1c653917cfe8d6fc6b5c664ad0fe38ed5496550
Kadir Can Köse <kadirkosedev@gmail.com>
2024-05-11 17:34:32 +0300

    particle eklendi bg eklendi

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class CameraBehaviour : MonoBehaviour
 {
@@ -10,6 +11,10 @@ public class CameraBehaviour : MonoBehaviour
 
     public bool isTransition = false;
     public float transitionFactor = 10f;
+
+    public GameObject HellParticle;
+    public GameObject SandParticle;
+    public GameObject IceParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +26,19 @@ public class CameraBehaviour : MonoBehaviour
     void LateUpdate()
     {
         this.gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, Levels[CurrentLevelIndex].transform.position + new Vector3(0,0,-10), transitionFactor * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject.Instantiate(HellParticle, GameObject.Find("character").transform.position, new Quaternion(), null);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            GameObject.Instantiate(SandParticle, GameObject.Find("character").transform.position, new Quaternion(), null);
+        }
+        if (Input.GetMouseButtonDown(2))
+        {
+            GameObject.Instantiate(IceParticle, GameObject.Find("character").transform.position, new Quaternion(), null);
+        }
     }
 
     public void ProceedNextLevel(int newCurrentLevelIndex)
